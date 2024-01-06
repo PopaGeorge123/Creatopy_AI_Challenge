3036fb2965c72683dafa9af3b760056964525cb4
George Popa <89342705+PopaGeorge123@users.noreply.github.com>
2024-01-06 18:41:41 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const AImodule = require('./generate-image');
 
 const app = express()
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors()); //ENABLE Cross Origin Requests
 
